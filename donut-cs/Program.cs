0e1d2f966d56c4fbee77db5965a3fd4381fba12c
YukiChriss <naoyuki.christopher.higaki@outlook.com>
2025-07-27 16:31:46 +0200

    Add project files.

---
+++ donut-cs/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace donut_cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            solution.RunDonut();
+        }
+    }
+}
